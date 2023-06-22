219c72509ba84952e899b65025c9d6f587f3a436
Swab Jul <zj.bulawan@gmail.com>
2023-06-22 17:05:35 +0800

    update

--- javascript-course/js/script.js
+++ javascript-course/js/script.js
@@ -2,11 +2,16 @@
 
 
 
-if (isMax) {
-  alert("The cart is full");
-  return;
-}
+let range = 5;
+let start = 1;
 
-if (isMin) {
+// // 0 to range
+// const r = Math.floor(Math.random() * range);
 
-}
+// // // inbetween
+const r = Math.floor(Math.random() * (range - start) + start);
+
+// start and range included
+// const r = Math.floor(Math.random() * (range - start + 1) + start);
+
+console.log(r);
