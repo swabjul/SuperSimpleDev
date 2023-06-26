3fb879037bf575f678c0911290755c3e77f80243
Swab Jul <zj.bulawan@gmail.com>
2023-06-26 17:49:40 +0800

    Update Exercise 9 DOM

--- javascript-course/js/script.js
+++ javascript-course/js/script.js
@@ -1,19 +1,10 @@
-function calcTotal() {
-  const inputElem = document.querySelector(".js-calc-input");
-  let total = 0;
 
-  if (isNaN(inputElem.value)) {
-    alert("Input is not a number");
-    inputElem.value = "aaaa";
-    return;
-  }
 
-  if (inputElem.value < 40) {
-    total = Number(inputElem.value) + 10;
-  } else {
-    total = inputElem.value;
-  }
 
-  document.querySelector(".js-calculate-result").innerText = `$${total}`;
 
+function nineB() {
+
+  const buttonElement = document.querySelector(".js-9b");
+  buttonElement.innerText = "9b done!";
+  
 }
