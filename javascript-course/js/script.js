99bf4a30433853b53798f8858e451b03a776f818
Swab Jul <zj.bulawan@gmail.com>
2023-06-27 14:21:12 +0800

    Update Exercise 9

--- javascript-course/js/script.js
+++ javascript-course/js/script.js
@@ -1,10 +1,28 @@
+let calculation = localStorage.getItem("calculator") || "";
 
-
-
-
-function nineB() {
-
-  const buttonElement = document.querySelector(".js-9b");
-  buttonElement.innerText = "9b done!";
-  
+function calc(num) {
+  calculation += num;
+  document.querySelector('.js-calculator').innerText = calculation;
+  localStorage.setItem("calculator", calculation)
 }
+
+function calculatorTotal() {
+  document.querySelector('.js-calculator').innerText = eval(calculation);
+  localStorage.setItem("calculator", calculation)
+}
+
+function resetCalculator() {
+  calculation = "";
+  localStorage.removeItem("calculator");
+  document.querySelector('.js-calculator').innerText = calculation;
+}
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelector('.js-calculator').innerText = calculation;
+})
