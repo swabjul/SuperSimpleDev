79343ed18c2546a40969f86c5fff0bbc16f6c057
Swab Jul <zj.bulawan@gmail.com>
2023-06-29 16:43:14 +0800

    Update lesson 10

--- javascript-course/js/script.js
+++ javascript-course/js/script.js
@@ -1,25 +1,15 @@
-function costCalcTotal() {
-  const inputElem = document.querySelector(".js-cst-input");
-  let cost = Number(inputElem.value);
+function toggle10E(selector) {
 
-  if (cost <= 0) {
-    const errorMessage = `Error: cost cannot be less than $0`
-    document.querySelector('.js-cst-total').innerHTML = `<span style="color:red">${errorMessage}</span>`
-    return;
+  const btnElem = document.querySelector(`.js-btn-toggle-${selector}`);
+  const isToggled = btnElem.classList.contains("is-toggled")
+
+
+  if (!isToggled) {
+    btnElem.classList.add("is-toggled");
+  } else {
+    btnElem.classList.remove("is-toggled");
   }
 
-  if (cost < 40) {
-    cost = Math.round((cost + 10) * 100) / 100;
-  } 
 
-  document.querySelector('.js-cst-total').innerText = cost;
-  inputElem.value = "";
 
-}
-
-function handlerKeyDownCst(event) {
-
-  if (event.key === "Enter") {
-    costCalcTotal();
-  }
 }
