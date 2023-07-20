cf2be82b90dc4c41279253db0170eefb22db4c69
Swab Jul <zj.bulawan@gmail.com>
2023-07-20 17:37:15 +0800

    Lesson 12

--- javascript-course/js/script.js
+++ javascript-course/js/script.js
@@ -1,15 +1,57 @@
-function toggle10E(selector) {
 
-  const btnElem = document.querySelector(`.js-btn-toggle-${selector}`);
-  const isToggled = btnElem.classList.contains("is-toggled")
-
-
-  if (!isToggled) {
-    btnElem.classList.add("is-toggled");
-  } else {
-    btnElem.classList.remove("is-toggled");
+const todoList = JSON.parse(localStorage.getItem("ToDoList")) || [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22"
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2022-12-22"
   }
+];
 
+console.log(todoList)
 
+function addTodoList() {
+  const nameElem = document.querySelector(".js-todo-input");
+  const dueDateElem = document.querySelector(".js-todo-date");
+
+  const name = nameElem.value;
+  const dueDate = dueDateElem.value;
+
+  todoList.push({name, dueDate});
+  renderTodoList();
+  localStorage.setItem("ToDoList", JSON.stringify(todoList));
 
 }
+
+
+function renderTodoList() {
+  const targetRenderElem = document.querySelector(".js-todo-list");
+  let todoListHTML = "";
+
+  todoList.forEach((value, index) => {
+    const todoObject = value;
+    const {name, dueDate} = todoObject;
+    const html = `
+      <p>
+        <span>${name}</span> 
+        <span>${dueDate}</span> 
+        <button class="btn-delete" onclick="
+        todoList.splice(${index}, 1);
+        localStorage.setItem('ToDoList', JSON.stringify(todoList));
+        renderTodoList();
+        ">Delete</button>
+      </p>
+    `;
+
+    todoListHTML += html;
+  });
+
+  targetRenderElem.innerHTML = todoListHTML;
+
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  renderTodoList();
+});
