6e5a2fece80f4ed44a112ffddc8b4e34740b4108
Swab Jul <zj.bulawan@gmail.com>
2023-07-24 17:24:19 +0800

    Upload Advance Funtion

--- javascript-course/js/script.js
+++ javascript-course/js/script.js
@@ -1,57 +1,15 @@
+function removeEggs(foods) {
+  let count = 0;
 
-const todoList = JSON.parse(localStorage.getItem("ToDoList")) || [
-  {
-    name: "make dinner",
-    dueDate: "2022-12-22"
-  },
-  {
-    name: "wash dishes",
-    dueDate: "2022-12-22"
-  }
-];
+  return foods.filter(value => {
+    if (value === "egg" && count < 2) {
+      count++
+      return false
+    } 
 
-console.log(todoList)
+    return value;
 
-function addTodoList() {
-  const nameElem = document.querySelector(".js-todo-input");
-  const dueDateElem = document.querySelector(".js-todo-date");
-
-  const name = nameElem.value;
-  const dueDate = dueDateElem.value;
-
-  todoList.push({name, dueDate});
-  renderTodoList();
-  localStorage.setItem("ToDoList", JSON.stringify(todoList));
-
-}
-
-
-function renderTodoList() {
-  const targetRenderElem = document.querySelector(".js-todo-list");
-  let todoListHTML = "";
-
-  todoList.forEach((value, index) => {
-    const todoObject = value;
-    const {name, dueDate} = todoObject;
-    const html = `
-      <p>
-        <span>${name}</span> 
-        <span>${dueDate}</span> 
-        <button class="btn-delete" onclick="
-        todoList.splice(${index}, 1);
-        localStorage.setItem('ToDoList', JSON.stringify(todoList));
-        renderTodoList();
-        ">Delete</button>
-      </p>
-    `;
-
-    todoListHTML += html;
   });
+};
 
-  targetRenderElem.innerHTML = todoListHTML;
-
-}
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  renderTodoList();
-});
+console.log(removeEggs(['egg', 'apple', 'egg', 'egg', 'ham']));
